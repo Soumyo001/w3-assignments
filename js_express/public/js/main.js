41c30280ce89d207ad9eda36475eaea21936d7c4
soumyo001 <www.soumyo@gmail.com>
2026-09-10 02:06:04 +0600

    refinement #2: gallery section, COMPLETE

--- js_express/public/js/main.js
+++ js_express/public/js/main.js
@@ -1,6 +1,7 @@
 import { initCarousel } from "./carousel.js";
 import { initNearby } from "./nearby.js";
 import { initGallery } from "./gallery.js";
+import { initHero } from "./hero.js";
 import { initDescription } from "./description.js";
 import { initPicker } from "./picker.js";
 import { initFavourites } from "./favourites.js";
@@ -9,6 +10,7 @@ import { initMap } from "./map.js";
 initCarousel();
 initNearby();
 initGallery();
+initHero();
 initDescription();
 initPicker();
 initFavourites();
