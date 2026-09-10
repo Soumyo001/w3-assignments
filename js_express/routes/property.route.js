7ae2cae3be1afa63cf128308d7915ddee749885c
Soumyo001 <www.soumyo@gmail.com>
2026-09-10 14:42:21 +0600

    repo separate

--- js_express/routes/property.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getPropertyController, getImagesController } from "../controllers/property.controller.js";
-
-const propertyRoutes = Router();
-
-propertyRoutes.route("/get-property").get(getPropertyController);
-propertyRoutes.route("/images").get(getImagesController);
-
-export default propertyRoutes;
